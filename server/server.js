af8df0d602c5f85ddb8f370e4b43843068ce9ad2
Ardhendu Singh <ardsingh@cisco.com>
2018-05-23 22:44:21 -0400

    Created generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -18,16 +19,8 @@ app.use(express.static(publicPath));
 // we are going to register for an event using io.on
 io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message) => {
         console.log('create message', message);
@@ -39,11 +32,7 @@ io.on('connection', (socket) => {
         // });
 
         // to send to everyone except to the socket from which it is received, do
-        socket.broadcast.emit('newMessage', {
-            from:message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
     });
     
     socket.on('disconnect', () => {
